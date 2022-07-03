45f9101f2cd9fa6d3727cc37acacee36739dba58
NextFutureOfficial <61616918+NextFutureOfficial@users.noreply.github.com>
2022-07-03 14:05:51 +0000

    update

---
+++ config/bot_config.py
@@ -0,0 +1,9 @@
+# This is the bot config file
+# You may need to set some tokens or other settings when you first install this bot.
+# After a right settings, you may better not to change all the things in this file unless you know what you are doing.
+
+# - Bot Settings
+# --- Bot Tokens
+bot_token = 'Enter your token here'
+# --- Bot Admin
+admin = 'Enter the admin telegram id'
