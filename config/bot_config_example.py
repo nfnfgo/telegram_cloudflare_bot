4485a35c52716247f4be63b7b66bb5f8f2f8d000
NextFutureOfficial <61616918+NextFutureOfficial@users.noreply.github.com>
2022-07-03 14:09:00 +0000

    update

---
+++ config/bot_config_example.py
@@ -0,0 +1,15 @@
+# This is the bot config file
+# You may need to set some tokens or other settings when you first install this bot.
+# After a right settings, you may better not to change all the things in this file unless you know what you are doing.
+
+# - Bot Settings
+# --- Bot Tokens
+token = 'Enter your token here'
+# --- Bot Admin
+admin = 'Enter the admin telegram id'
+
+
+
+# - Text Format Settings
+# --- separator, used to separate the text when bot send message to you
+sepa='————————————————'
