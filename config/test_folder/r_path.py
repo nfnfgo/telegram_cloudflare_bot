99c89c4ec8c8017c51ebb220786ef50f0b53886e
NextFutureOfficial <61616918+NextFutureOfficial@users.noreply.github.com>
2022-07-06 09:38:04 +0000

    update

--- config/test_folder/r_path.py
+++
@@ -1,3 +0,0 @@
-r_path='/workspaces/telegram_cloudflare_bot'
-timestamp='1657100042.7000992'
-readable_time='2022-07-06 09:34:02'
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
 import os
-
+import telebot
 from set_path.set_path import SetRPath
+from config import bot_config
 
+# set path
 SetRPath(os.getcwd())
 
+# some bot settings
